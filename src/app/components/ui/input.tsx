df0c64ce13924c2825575442b0307dfc84a559ac
lopesboa <lopesboa1@gmail.com>
2025-12-31 16:14:06 -0300

    feat: add input component

---
+++ src/app/components/ui/input.tsx
@@ -0,0 +1,26 @@
+import { Icon } from "@iconify-icon/react"
+
+type InputProps = React.DetailedHTMLProps<
+	React.InputHTMLAttributes<HTMLInputElement>,
+	HTMLInputElement
+> & {
+	iconName?: string
+}
+
+export function Input({ iconName, ...rest }: InputProps) {
+	return (
+		<div className="relative">
+			<input
+				className="w-full bg-black/50 border border-white/10 rounded-lg px-10 py-2.5 text-sm text-white placeholder:text-slate-600 focus:outline-none focus:border-indigo-500/50 focus:ring-1 focus:ring-indigo-500/50 transition-all"
+				{...rest}
+			/>
+			{iconName && (
+				<Icon
+					icon={iconName}
+					className="absolute left-3 top-1/2 -translate-y-1/2 text-slate-500 pointer-events-none"
+					width="18"
+				/>
+			)}
+		</div>
+	)
+}
