89d2e0560df7e67ad28dfac7eb2c803c09712520
lopesboa <lopesboa1@gmail.com>
2025-12-31 16:13:54 -0300

    feat: add dialog component

---
+++ src/app/components/ui/dialog.tsx
@@ -0,0 +1,39 @@
+import type { PropsWithChildren } from "react"
+import { useDialog } from "../../store/dialog"
+import { cn } from "../../utils/misc"
+
+export type DialogProps = PropsWithChildren & {
+	className?: string
+	id: string
+}
+
+export function Dialog({ children, className, id }: DialogProps) {
+	const { dialogs } = useDialog()
+	const openDialog = dialogs[id]
+
+	return (
+		<div
+			id="auth-modal"
+			className={cn(
+				"fixed inset-0 z-100",
+				className,
+				!openDialog ? " hidden" : "",
+			)}
+			aria-labelledby={id}
+			role="dialog"
+			aria-modal="true"
+		>
+			<div
+				className={cn(
+					"absolute inset-0 bg-black/80 backdrop-blur-sm transition-opacity duration-300",
+					!openDialog ? "opacity-0" : "",
+				)}
+				id="auth-modal-backdrop"
+			></div>
+
+			<div className="relative min-h-screen flex items-center justify-center p-4">
+				{children}
+			</div>
+		</div>
+	)
+}
