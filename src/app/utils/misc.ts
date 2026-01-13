7ecf23b869a1b66540db0a70529ef5fa6afa9ac4
lopesboa <lopesboa1@gmail.com>
2026-01-13 19:00:00 -0300

    feat: add create org slug fnc

--- src/app/utils/misc.ts
+++ src/app/utils/misc.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: clsx.ClassValue[]) {
 	return twMerge(clsx.clsx(inputs))
 }
+
+export function createOrgSlug(name: string): string {
+	return name
+		.toLowerCase()
+		.normalize("NFD")
+		.replace(/[\u0300-\u036f]/g, "")
+		.replace(/[^\w\s-]/g, "")
+		.trim()
+		.replace(/\s+/g, "-")
+		.replace(/-+/g, "-")
+		.replace(/^-|-$/g, "")
+}
