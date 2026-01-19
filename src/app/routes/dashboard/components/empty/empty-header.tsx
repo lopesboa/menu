e111fde8dbd4213694c6f514fdef8af666593ae2
lopesboa <lopesboa1@gmail.com>
2026-01-19 11:20:21 -0300

    feat: add empty header

---
+++ src/app/routes/dashboard/components/empty/empty-header.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/utils/misc"
+
+export function EmptyHeader({
+	className,
+	...props
+}: React.ComponentProps<"div">) {
+	return (
+		<div
+			data-slot="empty-header"
+			className={cn(
+				"flex max-w-sm flex-col items-center gap-2 text-center",
+				className,
+			)}
+			{...props}
+		/>
+	)
+}
