18e3b1ec05e5d2d54de5a423e686d3d9a2f17368
lopesboa <lopesboa1@gmail.com>
2026-01-20 00:47:45 -0300

    feat: add forgot page

--- src/app/routes/auth/forgot.tsx
+++ src/app/routes/auth/forgot.tsx
@@ -1,16 +1,68 @@
 import { Icon } from "@iconify-icon/react"
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
+import { Button } from "@/components"
+import { Field } from "@/components/form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react"
+import { type SubmitHandler, useForm } from "react-hook-form"
+import { authClient } from "@/lib/client"
+import { ForgotFormSchema } from "@/utils/user-validation"
+
+type ForgotForm = {
+	email: string
+	redirectTo?: string
+}
 
 export default function Forgot() {
+	const navigate = useNavigate()
+	const [loading, setLoading] = useState(false)
+
+	const {
+		handleSubmit,
+		register,
+
+		formState: { errors },
+	} = useForm<ForgotForm>({
+		resolver: zodResolver(ForgotFormSchema),
+		mode: "onBlur",
+		defaultValues: {
+			email: "",
+			redirectTo: "",
+		},
+	})
+
+	const onSubmit: SubmitHandler<ForgotForm> = async (data) => {
+		try {
+			setLoading(true)
+			await authClient.emailOtp.sendVerificationOtp(
+				{
+					email: data.email,
+					type: "forget-password",
+				},
+				{
+					onSuccess(ctx) {
+						navigate(data?.redirectTo || "/auth/verify", {
+							replace: true,
+							state: { redirectTo: "/auth/change-password" },
+						})
+					},
+				},
+			)
+		} catch (error) {
+		} finally {
+			setLoading(false)
+		}
+	}
+
 	return (
-		<div data-slot="auth-forgot" className="flex-col gap-6">
+		<div data-slot="auth-forgot" className="flex flex-col gap-6">
 			<Link
 				to="/auth/login"
 				className="absolute top-0 left-0 text-secondary hover:text-white transition-colors flex items-center gap-1 text-xs"
 			>
-				<Icon icon="solar:arrow-left-linear" /> Back
+				<Icon icon="solar:arrow-left-linear" /> Voltar
 			</Link>
-			<div className="text-center mt-6 mb-2 animate-slide-up-fade">
+			<div className="text-center  mb-2 animate-slide-up-fade">
 				<div className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-neutral-900 border border-border mb-4">
 					<Icon
 						icon="solar:lock-password-unlocked-bold-duotone"
@@ -18,37 +70,33 @@ export default function Forgot() {
 					/>
 				</div>
 				<h2 className="text-lg font-medium text-white tracking-tight">
-					Reset password
+					Recuperar senha
 				</h2>
 				<p className="text-xs text-secondary mt-1 max-w-65 mx-auto">
-					Enter the email associated with your account and we'll send you a
-					code.
+					Informe seu e-mail cadastrado e enviaremos as instruções para
+					redefinir sua senha.
 				</p>
 			</div>
 
-			<form className="flex flex-col gap-4 animate-slide-up-fade delay-100">
-				<div className="group relative">
-					<div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-						<Icon
-							icon="solar:letter-bold-duotone"
-							className="text-neutral-500 group-focus-within:text-white transition-colors"
-						/>
-					</div>
-					<input
-						type="email"
-						required
-						id="forgot-email"
-						placeholder="email@domain.com"
-						className="w-full bg-surface border border-border rounded-lg py-2.5 pl-10 pr-4 text-sm text-white placeholder-neutral-600 focus:outline-none focus:border-neutral-600 focus:ring-1 focus:ring-neutral-600 transition-all"
-					/>
-				</div>
+			<form
+				className="flex flex-col gap-4 animate-slide-up-fade delay-100"
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<Field
+					className="space-y-2"
+					label="Email"
+					errors={[errors.email?.message]}
+					inputProps={{
+						type: "email",
+						placeholder: "voce@exemplo.com",
+						iconName: "solar:letter-bold-duotone",
+						...register("email"),
+					}}
+				/>
 
-				<button
-					type="submit"
-					className="w-full bg-white text-black text-sm font-medium py-2.5 rounded-lg hover:bg-neutral-200 transition-colors shadow-[0_0_20px_-5px_rgba(255,255,255,0.3)]"
-				>
-					Send instructions
-				</button>
+				<Button loading={loading} type="submit">
+					Enviar Instruções
+				</Button>
 			</form>
 		</div>
 	)
