99f209f562cc7ce512b247d16a7f1b44cc875440
lopesboa <lopesboa1@gmail.com>
2026-01-20 00:22:43 -0300

    feat: add posthog event hook

---
+++ src/hooks/index.ts
@@ -0,0 +1,18 @@
+import type { CaptureOptions, EventName, Properties } from "posthog-js"
+import { usePostHog } from "posthog-js/react"
+
+export function usePostHogEvent() {
+	const posthog = usePostHog()
+
+	const capture = (
+		event_name: EventName,
+		properties?: Properties | null,
+		options?: CaptureOptions,
+	) => {
+		posthog.capture(event_name, properties, options)
+	}
+
+	return {
+		capture,
+	}
+}
