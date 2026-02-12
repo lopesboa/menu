f8c1b28f9fae0a8cafd7c58500ad46f9e548de22
lopesboa <lopesboa1@gmail.com>
2026-02-11 23:58:04 -0300

    refactor: adjust dashboard home

--- src/app/routes/dashboard/index.tsx
+++ src/app/routes/dashboard/index.tsx
@@ -4,7 +4,6 @@ import { cn } from "@/utils/misc"
 import { BottomNav } from "./components/layout/bottom-nav"
 import { Sidebar } from "./components/layout/sidebar"
 import { TopBar } from "./components/layout/top-bar"
-import { ToastContainer } from "./components/ui/toast"
 
 export default function Dashboard() {
 	return (
@@ -12,11 +11,10 @@ export default function Dashboard() {
 			<Sidebar />
 			<div className="flex flex-1 flex-col lg:ml-0">
 				<TopBar />
-				<main className={cn("flex-1 overflow-auto", "p-4 sm:p-6", "lg:pb-6")}>
+				<div className={cn("flex-1 overflow-auto", "p-4 sm:p-6", "lg:pb-6")}>
 					<Outlet />
-				</main>
+				</div>
 			</div>
-			<ToastContainer />
 			<BottomNav />
 		</div>
 	)
